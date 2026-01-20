92a58046ec3f67ba4263386feb5b92b779540cc6
abeyyaarkajal <kajalkukrejaagra@gmail.com>
2026-01-20 12:00:43 +0530

    Initial Commit

---
+++ src/main/java/com/example/super150_sms/Super150SmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.super150_sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Super150SmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Super150SmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/super150_sms/Super150SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.super150_sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Super150SmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
