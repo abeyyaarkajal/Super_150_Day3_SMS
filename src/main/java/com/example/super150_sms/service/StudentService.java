68119003c245f222c9d0b568fb1ecbe72983de8e
abeyyaarkajal <kajalkukrejaagra@gmail.com>
2026-01-27 15:01:47 +0530

    New Operations View and Update

--- src/main/java/com/example/super150_sms/service/StudentService.java
+++ src/main/java/com/example/super150_sms/service/StudentService.java
@@ -4,6 +4,8 @@ import com.example.super150_sms.model.StudentModel;
 import com.example.super150_sms.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
     private StudentRepository repository;
@@ -14,5 +16,21 @@ public class StudentService {
     //create
     public StudentModel addStudent(StudentModel student){
         return repository.save(student);
+    }
+
+    //display students
+    public List<StudentModel> getStudents(){
+        return repository.findAll();
+    }
+
+    //update
+    public StudentModel updateStudent(String id, StudentModel student){
+        StudentModel existingStudent = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No Student Found"));
+        existingStudent.setName(student.getName());
+        existingStudent.setAge(student.getAge());
+        existingStudent.setEmail(student.getEmail());
+
+        return repository.save(existingStudent);
     }
 }
